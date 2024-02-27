87d146e55c6b50d2f20c9738c8e778b881159e43
elzenaro <nam2626@naver.com>
2024-02-27 16:21:58 +0900

    삼항 연산자

--- Step03/src/E12_Quest.java
+++ Step03/src/E12_Quest.java
@@ -8,8 +8,25 @@ public class E12_Quest {
 		//단 뺄셈의 결과를 무조건 양수로 출력을 해야 합니다.
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("첫번째 숫자 입력 > ");
+		int n1 = sc.nextInt();
 		
+		System.out.println("두번째 숫자 입력 > ");
+		int n2 = sc.nextInt();
 		
+		//Case 1
+		System.out.println(n1 > n2 ? n1 - n2 : n2 - n1);
+		
+		//Case 2
+		int result = n1 - n2;
+		System.out.println(result < 0 ? -result : result);
 	}
 
 }
+
+
+
+
+
+
+
