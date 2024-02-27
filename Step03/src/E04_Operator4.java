06ce9a0fe3a02756ec581e22e94061b4df3d425f
elzenaro <nam2626@naver.com>
2024-02-27 11:03:18 +0900

    관계 연산자

---
+++ Step03/src/E04_Operator4.java
@@ -0,0 +1,28 @@
+
+public class E04_Operator4 {
+	/*
+	 * 		관계(비교) 연산자 --> 명제, 조건식
+	 * 			결과는 true/false로 나온다.
+	 * 
+	 * 		>	<	>=	<=	==	!=
+	 * 
+	 */
+	public static void main(String[] args) {
+		int n1 = 10, n2 = 7;
+		
+		System.out.println(n1 > n2);
+		System.out.println(n1 < n2);
+		System.out.println(n1 >= n2);
+		System.out.println(n1 <= n2);
+		System.out.println(n1 == n2);
+		System.out.println(n1 != n2);
+	}
+
+}
+
+
+
+
+
+
+
