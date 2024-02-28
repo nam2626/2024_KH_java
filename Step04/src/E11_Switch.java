a58bc5786df0a320ebe5ca2631b3852fb8d0f6d3
elzenaro <nam2626@naver.com>
2024-02-28 16:18:26 +0900

    switch

---
+++ Step04/src/E11_Switch.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class E11_Switch {
+	/*
+	 * 		밀크커피
+	 * 			프림 추가
+	 * 			설탕 추가
+	 * 			커피 추가
+	 * 			뜨거운 물 추가
+	 * 		설탕 커피
+	 * 			설탕 추가
+	 * 			커피 추가
+	 * 			뜨거운 물 추가
+	 * 		블랙 커피
+	 * 			커피 추가
+	 * 			뜨거운 물 추가
+	 * 
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. 블랙커피");
+		System.out.println("2. 설탕커피");
+		System.out.println("3. 밀크커피");
+		System.out.print("원하시는 번호를 입력 하세요 >> ");
+		int no = sc.nextInt();
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
