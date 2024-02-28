ee7970571971abfa97f9ca0a827944fec946b0a5
elzenaro <nam2626@naver.com>
2024-02-28 10:03:03 +0900

    if

--- Step04/src/E01_if.java
+++ Step04/src/E01_if.java
@@ -1,6 +1,18 @@
 
 public class E01_if {
-
+	/*
+	 * 		if문
+	 * 			조건식이 true면 if문에 붙어있는 { 실행영역 }에 있는 코드를 실행
+	 * 			조건식이 false면 해당 if문을 건너뜀
+	 * 
+	 * 		조건식 : 관계연산자와 논리연산자를 이용한 수식, 결과는 true/false로 나오는 수식
+	 * 
+	 * 		if(조건식){
+	 * 			실행할 코드...	
+	 * 			실행할 코드...	
+	 * 			실행할 코드...	
+	 * 		}
+	 */
 	public static void main(String[] args) {
 		int n = 0;
 		
