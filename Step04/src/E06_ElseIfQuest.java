768328f04a747ab0efb957aa7214a1cb265e318a
elzenaro <nam2626@naver.com>
2024-02-28 12:09:28 +0900

    else if

--- Step04/src/E06_ElseIfQuest.java
+++ Step04/src/E06_ElseIfQuest.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
 
 public class E06_ElseIfQuest {
+	/*
+	  	사용자로부터 점수를 입력받아, 다음의 기준으로 등급을 분류하여 출력하는 프로그램을 작성하세요.
 
+		90점 이상: A
+		80점 이상 ~ 90점 미만: B
+		70점 이상 ~ 80점 미만: C
+		60점 이상 ~ 70점 미만: D
+		60점 미만: F
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String grade = "F";
+		System.out.print("점수 입력 > ");
+		int score = sc.nextInt();
+		
+		if(score >= 90)
+			grade =	"A";
+		else if(score >= 80)
+			grade =	"B";
+		else if(score >= 70)
+			grade =	"C";
+		else if(score >= 60)
+			grade =	"D";
+		
+		System.out.println(grade);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
