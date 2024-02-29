d55a90dfec44e5c244a2a5c18f5a10a913938428
elzenaro <nam2626@naver.com>
2024-02-29 10:44:00 +0900

    while

---
+++ Step05/src/E01_While.java
@@ -0,0 +1,27 @@
+
+public class E01_While {
+	/*
+	 * 		while(조건식){
+	 * 			실행할 코드
+	 * 			실행할 코드
+	 *			.... 
+	 * 		}
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		int i = 5;
+		while(i > 0) {
+			System.out.println("안녕하세요");
+			i--;
+		}
+	}
+
+}
+
+
+
+
+
+
+
---
+++ Step05/src/E02_While.java
@@ -0,0 +1,26 @@
+
+public class E02_While {
+
+	public static void main(String[] args) {
+		//숫자 1~100까지 출력
+		int n = 1;
+		
+		//case 1
+		while(n < 101) {
+			System.out.print(n + " ");
+			n++;
+		}
+		
+		//case 2
+		n = 0;
+		while() {
+			System.out.print(n + " ");
+			
+		}
+	}
+
+}
+
+
+
+
