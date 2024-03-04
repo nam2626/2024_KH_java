77e01620b8f933f5900b5391ed9c80141d958503
elzenaro <nam2626@naver.com>
2024-03-04 12:14:56 +0900

    문제 풀이

---
+++ Step06/src/E05_Quest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class E05_Quest {
+	/*
+	 * 	숫자 0을 입력 할때까지 숫자를 입력 받은 후
+	 *  입력한 숫자들 중 홀수만 출력
+	 *  반드시 String 타입의 변수를 사용
+	 *  
+	 *  실행 예시>
+	 *  숫자 입력 > 11
+	 *  숫자 입력 > 4
+	 *  숫자 입력 > 3
+	 *  숫자 입력 > 2
+	 *  숫자 입력 > 0
+	 *  
+	 *  홀수 목록 : 11 3
+	 */
+	public static void main(String[] args) {
+		String result = "홀수 목록 : ";
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println(result);
+	}
+
+}
