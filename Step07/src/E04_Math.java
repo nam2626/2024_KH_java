85c15473cf7ba5ebe7ddc89f2205e772d76be92a
elzenaro <nam2626@naver.com>
2024-03-05 11:09:27 +0900

    Math

---
+++ Step07/src/E04_Math.java
@@ -0,0 +1,28 @@
+
+public class E04_Math {
+
+	public static void main(String[] args) {
+		//절대값
+		System.out.println(Math.abs(-100));
+		//올림
+		System.out.println(Math.ceil(3.4));//4
+		System.out.println(Math.ceil(-3.4));//-3
+		//내림
+		System.out.println(Math.floor(3.4)); //3
+		System.out.println(Math.floor(-3.4));//-4
+		//반올림
+		System.out.println(Math.round(3.4));//3
+		System.out.println(Math.round(-3.4));//-3
+		//큰 값, 작은 값
+		System.out.println(Math.max(10, 20));
+		System.out.println(Math.min(10, 20));
+	}
+
+}
+
+
+
+
+
+
+
