6d26cdbde5ec84dea48a68c63daa8d535f51333c
elzenaro <nam2626@naver.com>
2024-03-05 13:54:15 +0900

    Math

--- Step07/src/E06_Quest.java
+++ Step07/src/E06_Quest.java
@@ -1,9 +1,20 @@
 
 public class E06_Quest {
-
+	/*
+	 * 삼각함수 표 출력
+	 * 각도		사인		코사인	탄젠트
+	 * 1		0.000	0.000	0.000
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=1;i<91;i++) {
+			double radian = Math.toRadians(i);
+			if(i < 90)
+			System.out.printf("%4d %10.4f %10.4f %10.4f\n",
+					i,Math.sin(radian), Math.cos(radian), Math.tan(radian));
+			else
+				System.out.printf("%4d %10.4f %10.4f %10s\n",
+						i,Math.sin(radian), Math.cos(radian),"∞");
+		}
 	}
 
 }
