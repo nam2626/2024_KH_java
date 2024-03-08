bb4b43ac32cf3fef327b539301c2115cc3dd52a8
elzenaro <nam2626@naver.com>
2024-03-08 10:58:19 +0900

    배열

--- Step09/src/E10_Quest.java
+++ Step09/src/E10_Quest.java
@@ -23,7 +23,7 @@ public class E10_Quest {
 				//배열에 저장할 공간이 없으면 길이를 5씩 늘림
 				if(idx == arr.length) {
 					int[] temp = new int[arr.length+5];
-					//배열 복사
+					//배열 복사(복사할 원본 배열, 복사할 시작 인덱스번호, 복사 받을 배열, 복사 받을 시작 위치, 복사할 개수)
 					System.arraycopy(arr, 0, temp, 0, arr.length);
 					arr = temp;
 				}
@@ -32,7 +32,26 @@ public class E10_Quest {
 				arr[idx] = sc.nextInt();
 				sc.nextLine();
 				idx++;
+			}else if(command.equals("del")) {
+				//삭제 처리
+				//삭제할 숫자 입력 받아서 삭제
+				System.out.print("삭제할 숫자 입력 > ");
+				int n = sc.nextInt();
+				sc.nextLine();
+				
+				for(int i=0;i<idx;i++) {
+					//빈자리 없이 메꾸는 작업
+					if(arr[i] == n) {
+						for(int j=i;j<idx-1;j++) {
+							arr[j] = arr[j+1];
+						}
+						idx--;
+					}
+				}
+			}else if(command.equals("all")) {
 				System.out.println(Arrays.toString(arr));
+			}else {
+				System.out.println("잘못된 명령어를 입력하셨습니다. 다시입력해주세요");
 			}
 			
 		}//end - while
