284bac68de55148698c774a3c96b5483147b0a5b
elzenaro <nam2626@naver.com>
2024-03-11 16:12:06 +0900

    정렬

---
+++ Step10/src/E04_Sort.java
@@ -0,0 +1,29 @@
+
+public class E04_Sort {
+	//전체 배열을 출력하는 메서드
+	public static void printArray(int[] arr, int pass) {
+		System.out.print(pass + " pass : ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void bubleSort(int[] arr) {
+		for(int i=0;i<arr.length-1;i++) { //pass
+			for(int j=0;j<arr.length-1-i;j++) {//실제 정렬을 수행하는 부분
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			printArray(arr, i+1);
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = new int[] {5,7,9,3,1};
+		bubleSort(arr);
+	}
+
+}
