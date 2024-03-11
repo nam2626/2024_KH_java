48f4a136f08fc655b38569fbbe80b306205062a2
elzenaro <nam2626@naver.com>
2024-03-11 12:25:52 +0900

    메서드

--- Step10/src/E02_Quest.java
+++ Step10/src/E02_Quest.java
@@ -1,13 +1,18 @@
 
 public class E02_Quest {
-
+	//랜덤으로 매개변수로 받은 범위로 배열에 저장
+	public static void initArray(int[] arr, int min, int max) {
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = (int)(Math.random() * (max - min + 1)) + 1;
+		}
+	}
+	//배열을 하나 매개변수 받아서 홀수 개수를 리턴하는 메서드
+	
 	public static void main(String[] args) {
 		//길이가 10인 정수형 배열 생성
 		int[] arr = new int[10];
 		//랜덤으로 숫자 1~20 범위로 생성한 배열에 저장
-		for(int i=0;i<arr.length;i++) {
-			arr[i] = (int)(Math.random() * 20) + 1;
-		}
+		initArray(arr,1,20);
 		//배열에 저장된 내용 중 홀수 개수를 출력
 		int count = 0;
 		for(int i=0;i<arr.length;i++) {
