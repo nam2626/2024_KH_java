b09b18fc27764002b0ad6c733c728abe5036fed6
elzenaro <nam2626@naver.com>
2024-03-12 14:11:56 +0900

    클래스

---
+++ Step11/src/e02_method/Person.java
@@ -0,0 +1,13 @@
+package e02_method;
+
+public class Person {
+	String name;
+	int age;
+	
+	//Person의 정보를 출력하는 메서드
+	//출력 형태 --> 이름 나이
+	
+	void printPersonInfo() {
+		System.out.println(name + " " + age);
+	}
+}
