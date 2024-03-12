5f83b72a5382b90f0aac7c2db6f578895311bc75
elzenaro <nam2626@naver.com>
2024-03-12 14:42:18 +0900

    클래스

--- Step11/src/e02_method/Person.java
+++ Step11/src/e02_method/Person.java
@@ -6,8 +6,27 @@ public class Person {
 	
 	//Person의 정보를 출력하는 메서드
 	//출력 형태 --> 이름 나이
-	
 	void printPersonInfo() {
-		System.out.println(name + " " + age);
+		System.out.println("이름 : "+name);
+		System.out.println("나이 : "+age);
+	}
+	
+	//데이터를 받아서 초기화하는 메서드
+	void init(String n, int a) {
+		if(!n.equals("") || n != null)
+			name = n;
+		else
+			name = "임시값";
+		
+		if(a > 0)
+			age = a;
 	}
 }
+
+
+
+
+
+
+
+
---
+++ Step11/src/e02_method/PersonMain.java
@@ -0,0 +1,15 @@
+package e02_method;
+
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.name = "홍길동";
+		p1.age = 20;
+		p1.printPersonInfo();
+		
+		
+	}
+
+}
