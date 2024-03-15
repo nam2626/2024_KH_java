eb1e46629afbd446080c64b5cdf39da3d592046d
elzenaro <nam2626@naver.com>
2024-03-15 12:16:42 +0900

    String

--- Step12/src/e01_string_ex/StringEx.java
+++ Step12/src/e01_string_ex/StringEx.java
@@ -25,8 +25,38 @@ public class StringEx {
 		//문자열 오른쪽 끝에서부터 검색 - 검색 결과는 인덱스 번호, 결과가 없으면 -1
 		System.out.println(str3.lastIndexOf("World"));
 		System.out.println(str3.lastIndexOf("World",22));
+		//알파벳 전부 소문자로 변환
+		System.out.println(str3.toLowerCase());
+		//알파벳 전부 대문자로 변환
+		System.out.println(str3.toUpperCase());
+		//글자 길이 리턴
+		System.out.println(str3.length());
+		//문자열 부분 추출 - 10번 인덱스부터 마지막 인덱스까지 문자열을 잘라내서 리턴 
+		System.out.println(str3.substring(10));
+		//문자열 부분 추출 - 10번 인덱스부터 15 인덱스전까지 문자열을 잘라내서 리턴 
+		System.out.println(str3.substring(10,15));
+		//문자열 바꾸기
+		System.out.println(str3.replace("World", "Hell"));
 		
+		String str4 = "        Hello World      ";
+		//문자열 개수랑 문자열 출력
+		System.out.println(str4.length());
+		System.out.println(str4);
+		//문자열 양쪽에 필요없는 공백을 제거
+		System.out.println(str4.trim());
+		System.out.println(str4.trim().length());
 		
+		String str5 = "java,html,css,javascript,jsp,spring";
+		//문자열을 구분자를 기준으로 잘라서 배열로 만들어줌
+		String[] result = str5.split(",");
+		System.out.println(Arrays.toString(result));
+		System.out.println(String.join("/", result));
+		
+		//문자열 포멧 : %s, %d, %f, %o, %x %X, %b, %c
+		System.out.println(String.format("이름 : %s, 나이 : %d", "김철수",20));
+		System.out.println(String.format("%d %X %o", 10,10,10));
+		System.out.println(String.format("%b %c", true, 'F'));
+		System.out.println(String.format("%f %g", 3.1415,3.1415));
 		
 	}
 }
