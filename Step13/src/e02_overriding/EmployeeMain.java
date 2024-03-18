4db34b6a4f1acab7f3ae8564e509b06bb22310bc
elzenaro <nam2626@naver.com>
2024-03-18 11:14:04 +0900

    상속

--- Step13/src/e02_overriding/EmployeeMain.java
+++ Step13/src/e02_overriding/EmployeeMain.java
@@ -1,10 +1,20 @@
 package e02_overriding;
 
 public class EmployeeMain {
-
+	
+	public static void work(Employee e) {
+		e.work();
+	}
+	
 	public static void main(String[] args) {
-		//Employee, SalaryEmployee, DispatchEmployee 
-		//생성 후 work 실행
+		Employee e1 = new Employee();
+		SalaryEmployee e2 = new SalaryEmployee();
+		DispatchEmployee e3 = new DispatchEmployee();
+		
+		work(e1);
+		work(e2);
+		work(e3);
+		
 	}
 
 }
