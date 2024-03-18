3d93193ebef68e753ea5d96d0c96bef3d7a4bceb
elzenaro <nam2626@naver.com>
2024-03-18 10:02:43 +0900

    상속

---
+++ Step13/src/e01_inheritance/Person.java
@@ -0,0 +1,5 @@
+package e01_inheritance;
+
+public class Person extends Animal {
+
+}
---
+++ Step13/src/e01_inheritance/PersonMain.java
@@ -0,0 +1,9 @@
+package e01_inheritance;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
