8f53fc89583d89ff4e4421365f25bce93d0230e1
elzenaro <nam2626@naver.com>
2024-03-20 14:43:59 +0900

    Abstract

---
+++ Step15/src/e01_abstract/AbstractMain.java
@@ -0,0 +1,12 @@
+package e01_abstract;
+
+public class AbstractMain {
+
+	public static void main(String[] args) {
+//		Animal animal = new Animal();
+		Person p = new Person();
+		p.run();
+		p.eat();
+	}
+
+}
---
+++ Step15/src/e01_abstract/Person.java
@@ -0,0 +1,16 @@
+package e01_abstract;
+
+public class Person extends Animal {
+
+	public Person() {
+		System.out.println("Person 생성자");
+	}
+
+	//추상 메서드는 반드시 오버라이딩 해줘야됨
+	//오버라이딩을 안할경우에는 해당 클래스도 추상 클래스로 작성
+	@Override
+	public void run() {
+		System.out.println("사람이 달립니다.");
+	}
+
+}
