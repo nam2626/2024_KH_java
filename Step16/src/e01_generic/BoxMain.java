b69c1bf4b919fbadea48e9cdaeeb58508b41d99d
elzenaro <nam2626@naver.com>
2024-03-21 14:52:51 +0900

    Generic

--- Step16/src/e01_generic/BoxMain.java
+++ Step16/src/e01_generic/BoxMain.java
@@ -8,7 +8,19 @@ public class BoxMain {
 		box.setObj(200);
 		System.out.println(box.getObj());
 		
+		// Object로 저장하면 어떤 타입이든지 될 수 있어서
+		// 사용할 때는 반드시 형변환 부분이 필요하다
+		box.setObj(new Apple());
+		//getObj() 해서 printApple을 실행
+		Apple apple = (Apple) box.getObj();
+		apple.printApple();
+		
+		box.setObj(new Orange());
+		((Orange)box.getObj()).printOrange();
 		
 	}
 
 }
+
+
+
