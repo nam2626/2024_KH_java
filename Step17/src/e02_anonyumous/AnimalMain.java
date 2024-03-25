2e18674753330adb49994781eb3f8298749b24b5
elzenaro <nam2626@naver.com>
2024-03-25 12:15:53 +0900

    익명 클래스

--- Step17/src/e02_anonyumous/AnimalMain.java
+++ Step17/src/e02_anonyumous/AnimalMain.java
@@ -1,10 +1,11 @@
 package e02_anonyumous;
 
 public class AnimalMain {
-
+	public static void eat(Animal animal) {
+		animal.eat();
+	}
 	public static void main(String[] args) {
 		Animal animal = new Animal() {
-			
 			@Override
 			public void eat() {
 				System.out.println("동물이 먹이를 먹습니다.");
@@ -12,6 +13,24 @@ public class AnimalMain {
 		};
 		
 		animal.eat();
+		
+		Animal person = new Animal() {
+			@Override
+			public void eat() {
+				System.out.println("사람이 밥을 먹습니다.");
+			}
+		};
+		person.eat();
+		
+		eat(animal);
+		eat(person);
+		eat(new Animal() {
+			
+			@Override
+			public void eat() {
+				System.out.println("고양이가 츄르를 먹습니다.");
+			}
+		});
 	}
 
 }
