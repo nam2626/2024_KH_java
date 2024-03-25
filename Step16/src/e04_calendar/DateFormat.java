5099a71665758d7e20f3706a49152fc7a332e6d2
elzenaro <nam2626@naver.com>
2024-03-25 10:11:33 +0900

    날짜 서식

--- Step16/src/e04_calendar/DateFormat.java
+++ Step16/src/e04_calendar/DateFormat.java
@@ -7,7 +7,7 @@ public class DateFormat {
 
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
-		
+		cal.set(Calendar.HOUR, 13);
 		//날짜 서식 문자를 이용해서 원하는 형태로 날짜를 문자열로 만들어 주는 클래스
 		SimpleDateFormat sdf = new SimpleDateFormat();
 		sdf.applyPattern("yyyy");//연도 4자리
@@ -28,6 +28,19 @@ public class DateFormat {
 		System.out.println(sdf.format(cal.getTime()));
 		sdf.applyPattern("E");//요일
 		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("EEE");//요일 약자(영문은 3자리) --> 로컬 형식
+		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("EEEE");//요일 풀네임 --> 로컬 형식
+		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("H"); //시간 1자리 - 24시간 기준
+		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("HH");//시간 2자리 - 24시간 기준
+		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("h"); //시간 1자리 - 12시간 기준
+		System.out.println(sdf.format(cal.getTime()));
+		sdf.applyPattern("hh");//시간 2자리 - 12시간 기준
+		System.out.println(sdf.format(cal.getTime()));
+		
 		
 		
 	}
