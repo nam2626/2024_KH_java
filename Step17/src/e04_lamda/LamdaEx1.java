0089bd6891fced7c54bd184dc4395330c7a5c022
elzenaro <nam2626@naver.com>
2024-03-25 14:40:55 +0900

    익명 클래스

--- Step17/src/e04_lamda/LamdaEx1.java
+++ Step17/src/e04_lamda/LamdaEx1.java
@@ -4,7 +4,13 @@ public class LamdaEx1 {
 
 	public static void main(String[] args) {
 		//Calcurator 익명클래스로 만들어서 덧셈하는 기능을 작성
-		
+		Calcurator sum = new Calcurator() {
+			@Override
+			public int calc(int x, int y) {
+				return x + y;
+			}
+		};
+		System.out.println(sum.calc(10, 20));
 	}
 
 }
