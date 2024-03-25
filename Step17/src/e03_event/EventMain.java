1fe4e76f8b72c5620379ac2de970785c1c9f8496
elzenaro <nam2626@naver.com>
2024-03-25 14:37:59 +0900

    익명 클래스

--- Step17/src/e03_event/EventMain.java
+++ Step17/src/e03_event/EventMain.java
@@ -4,7 +4,20 @@ public class EventMain {
 
 	public static void main(String[] args) {
 		Button btnRegister = new Button("회원가입");
+		btnRegister.setOnClickListener(new ButtonRegisterOnClickEvent());
+		btnRegister.onClick();
 		
+		Button btnLogin = new Button("로그인");
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				System.out.println(view.name + " 버튼 클릭");
+				System.out.println("로그인 처리");
+			}
+		});
+		
+		btnLogin.onClick();
 		
 	}
 
---
+++ Step17/src/e04_lamda/LamdaEx1.java
@@ -0,0 +1,10 @@
+package e04_lamda;
+
+public class LamdaEx1 {
+
+	public static void main(String[] args) {
+		//Calcurator 익명클래스로 만들어서 덧셈하는 기능을 작성
+		
+	}
+
+}
