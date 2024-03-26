d01b58a532f187c532fab269bd0c52fccf42bf5c
elzenaro <nam2626@naver.com>
2024-03-26 10:53:47 +0900

    Thread

---
+++ Thread/src/e01_init/RunnableMain.java
@@ -0,0 +1,9 @@
+package e01_init;
+
+public class RunnableMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Thread/src/e01_init/RunnableRun.java
+++ Thread/src/e01_init/RunnableRun.java
@@ -1,5 +1,11 @@
 package e01_init;
 
-public class RunnableRun {
+public class RunnableRun implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=0;i<1000000;i++);
+		System.out.println(Thread.currentThread().getName() + " - 종료");
+	}
 
 }
