df7bf941bb9e8c5b750dcbd69c1cb39e7ce580c3
elzenaro <nam2626@naver.com>
2024-03-26 11:38:59 +0900

    Thread

--- Thread/src/e01_init/RunnableRun.java
+++ Thread/src/e01_init/RunnableRun.java
@@ -12,7 +12,7 @@ public class RunnableRun implements Runnable{
 	
 	@Override
 	public void run() {
-		for(int i=0;i<1000000;i++);
+		for(int i=0;i<100000000;i++);
 		System.out.println(Thread.currentThread().getName() + " - 종료 " + getTime());
 	}
 
