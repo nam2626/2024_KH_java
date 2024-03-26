80b2abf9794481b84f0e3b3024fb544740113622
elzenaro <nam2626@naver.com>
2024-03-26 15:20:47 +0900

    Thread

---
+++ Thread/src/e04_join/DiceMain.java
@@ -0,0 +1,17 @@
+package e04_join;
+
+import java.util.ArrayList;
+
+public class DiceMain {
+
+	public static void main(String[] args) {
+		ArrayList<DiceThread> list = new ArrayList<DiceThread>();
+		
+		//DiceThread 5개 생성해서 리스트에 저장
+		
+		//리스트에 저장한 스레드 실행
+		
+		//DiceThread에 있는 getAverage 실행해서 평균 횟수를 출력
+	}
+
+}
