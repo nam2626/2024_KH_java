3beac7e75c6f59358f915c5daac428ee4f248eaa
elzenaro <nam2626@naver.com>
2024-03-26 15:16:01 +0900

    Thread

--- Thread/src/e04_join/DiceThread.java
+++ Thread/src/e04_join/DiceThread.java
@@ -1,9 +1,24 @@
 package e04_join;
 
 import java.util.Random;
+import java.util.Vector;
 
 public class DiceThread extends Thread{
-
+	private static Vector<Integer> list = new Vector<Integer>();
+	
+	public static double getAverage() {
+		//벡터에 저장된 숫자들의 평균을 리턴
+//		int total = 0;
+//		
+//		for(int i=0;i<list.size();i++)
+//			total += list.get(i);
+//		
+//		return (double)total / list.size();
+		
+		return list.parallelStream().reduce(0, (acc, curr) -> acc += curr)
+				/ (double)list.size();
+	}
+	
 	public DiceThread(String name) {
 		super(name);
 	}
@@ -21,9 +36,15 @@ public class DiceThread extends Thread{
 		
 		while(true) {
 			//주사위(1~6)를 두번 굴려서
-			//각 주사위가 전부 6이 나올때까 굴림
+			int dice1 = r.nextInt(6)+1;
+			int dice2 = r.nextInt(6)+1;
 			//주사위를 굴릴때마다 주사위를 굴린 횟수를 count에 저장
+			count++;
+			//각 주사위가 전부 6이 나올때까 굴림
+			if(dice1 + dice2 == 12)
+				break;
 		}
+		System.out.println(getName() + "@" + count);
 	}
 	
 }
