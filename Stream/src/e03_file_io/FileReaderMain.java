7952302d94a37598662e099ccb577cd5ab3cc1d6
elzenaro <nam2626@naver.com>
2024-03-27 14:47:11 +0900

    파일 출력

---
+++ Stream/src/e03_file_io/FileReaderMain.java
@@ -0,0 +1,37 @@
+package e03_file_io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderMain {
+
+	public static void main(String[] args) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("hello.txt");
+			br = new BufferedReader(fr);
+
+			while (true) {
+				String str = br.readLine();
+				if (str == null)
+					break;
+				System.out.println(str);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
