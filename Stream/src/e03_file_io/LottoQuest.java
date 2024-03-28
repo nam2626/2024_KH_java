6434d4de855795b1a3e36c803b83b9f66b2f0c3b
elzenaro <nam2626@naver.com>
2024-03-28 10:21:02 +0900

    로또번호 생성까지 완료

--- Stream/src/e03_file_io/LottoQuest.java
+++ Stream/src/e03_file_io/LottoQuest.java
@@ -1,6 +1,11 @@
 package e03_file_io;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Random;
 import java.util.TreeSet;
 
@@ -18,11 +23,48 @@ public class LottoQuest {
 		
 		return list;
 	}
+	public static String makeLottoForm(ArrayList<TreeSet<Integer>> list) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("%13s\n", "LOTTO"));
+		sb.append("---------------------\n");
+		//로또번호 넣음
+		//숫자 무조건 2자리, 숫자간의 띄워쓰기 하나
+		list.forEach(t -> {
+			sb.append("  ");
+			//한셋트 문자열로 붙임
+			t.forEach(n -> sb.append(String.format("%2d ", n)));
+			sb.append("\n");
+		});
+		sb.append("---------------------\n");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		sb.append(sdf.format(Calendar.getInstance().getTime()));
+		return sb.toString();
+	}
+	
+	public static void fileWrite(String result) {
+		try(FileWriter fw = new FileWriter("lotto.txt");
+				PrintWriter pw = new PrintWriter(fw,true);){
+			pw.println(result);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(createLotto(5));
+		ArrayList<TreeSet<Integer>> list = createLotto(5);
+		String r = makeLottoForm(list);
+		System.out.println(r);
+		fileWrite(r);
+		
 	}
 
 }
+
+
+
+
+
+
 
 
 
