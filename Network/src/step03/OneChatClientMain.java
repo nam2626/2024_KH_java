e20e46f02ab56fe572b48bd71a39ca541271b247
elzenaro <nam2626@naver.com>
2024-03-28 16:10:23 +0900

    소켓 프로그래밍

--- Network/src/step03/OneChatClientMain.java
+++ Network/src/step03/OneChatClientMain.java
@@ -4,25 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
-
-public class OneChatServerMain {
+ 
+public class OneChatClientMain {
 	public static void main(String[] args) {
-		try (ServerSocket server = new ServerSocket(1234); 
+		try (Socket server = new Socket("127.0.0.1",1234); 
 				Scanner sc = new Scanner(System.in);) {
-			System.out.println("서버가 오픈되었습니다. 클라이언트 접속 대기중....");
-			Socket client = server.accept();
-			System.out.println(client.getInetAddress() + " 클라이언트가 접속하였습니다.");
+			System.out.println("서버 접속 완료");
 			//-----------------------------------
 			Thread t = new Thread(() -> {
 				try(BufferedReader br = new BufferedReader(
-						new InputStreamReader(client.getInputStream()))){
+						new InputStreamReader(server.getInputStream()))){
 					while(true) {
 						String msg = br.readLine();
 						if(msg.equals("exit")) break;
-						System.out.println(client.getInetAddress() + " 님의 메세지 " + msg);
+						System.out.println(server.getInetAddress() + " 님의 메세지 " + msg);
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -30,7 +27,7 @@ public class OneChatServerMain {
 			});
 			t.start();
 			//-----------------------------------
-			PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
+			PrintWriter pw = new PrintWriter(server.getOutputStream(), true);
 
 			while (true) {
 				System.out.println("메세지 입력 : ");
