cbb0aad075f8f824f53f860962e47070ba92879d
elzenaro <nam2626@naver.com>
2024-04-15 16:18:42 +0900

    jdbc

--- jdbc/src/config/DBManager.java
+++ jdbc/src/config/DBManager.java
@@ -1,16 +1,19 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import oracle.jdbc.pool.OracleDataSource;
 
 public class DBManager {
 	private static DBManager instance = new DBManager();
 	private OracleDataSource ods;
-	
+
 	private DBManager() {
 		try {
-			//OracleDataSource 생성 및 초기화
+			// OracleDataSource 생성 및 초기화
 			ods = new OracleDataSource();
 			String url = "jdbc:oracle:thin:@127.0.0.1:1521/xe";
 			String user = "c##scott";
@@ -21,16 +24,29 @@ public class DBManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static DBManager getInstance() {
-		if(instance == null)
+		if (instance == null)
 			instance = new DBManager();
 		return instance;
 	}
 
-	
-	
-	
+	public Connection getConnection() throws SQLException {
+		return ods.getConnection();
+	}
+
+	public void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
