f82dc1ccb1f89b977feab45da55bdb5c44328f95
elzenaro <nam2626@naver.com>
2024-04-16 15:38:20 +0900

    StudentProject JDBC

--- StudentProject_JDBC/src/controller/PrintAllController.java
+++ StudentProject_JDBC/src/controller/PrintAllController.java
@@ -12,9 +12,9 @@ public class PrintAllController implements Controller {
 		System.out.println("전체 학생정보를 출력합니다........");
 		ArrayList<StudentDTO> list = 
 				StudentService.getInstance().selectAllStudent();
-//		for(int i=0;i<list.size();i++) {
-//			list.get(i).printInfo();
-//		}
+		for(int i=0;i<list.size();i++) {
+			list.get(i).printStudentInfo();
+		}
 		System.out.println();
 	}
 }
--- StudentProject_JDBC/src/dao/StudentDAO.java
+++ StudentProject_JDBC/src/dao/StudentDAO.java
@@ -13,56 +13,56 @@ import oracle.jdbc.pool.OracleDataSource;
 public class StudentDAO {
 	private static StudentDAO instance = null;
 	private OracleDataSource ods;
-	
+
 	private StudentDAO() {
-		//ods 접속 정보 이용해서 초기화
+		// ods 접속 정보 이용해서 초기화
 		try {
 			ods = new OracleDataSource();
-			//url, user, passwd 셋팅
+			// url, user, passwd 셋팅
 			ods.setURL(DBConfig.DB_URL);
 			ods.setUser(DBConfig.DB_USER);
 			ods.setPassword(DBConfig.DB_PASSWD);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static StudentDAO getInstance() {
-		if(instance == null)
+		if (instance == null)
 			instance = new StudentDAO();
 		return instance;
 	}
 
-	public ArrayList<StudentDTO> selectAllStudent(){
-		//학생 정보를 전부 읽어오는 작업
-		//학번 이름 학과명 평점 성별 --> 학과, 학생 테이블 조인해서 sql문 실행
-		String sql = "SELECT S.STD_NO, S.STD_NAME, "
-				+ "M.MAJOR_NAME, S.STD_SCORE, S.STD_GENDER "
-				+ "FROM STUDENT S JOIN MAJOR M "
-				+ "ON S.MAJOR_NO = M.MAJOR_NO";
-		
-		try(Connection conn = ods.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement(sql);){
-			
-			try(ResultSet rs = pstmt.executeQuery()){
-				while(rs.next()) {
-					System.out.println(rs.getString(1) + " " + rs.getString(2));
+	public ArrayList<StudentDTO> selectAllStudent() {
+		// 학생 정보를 전부 읽어오는 작업
+		// 학번 이름 학과명 평점 성별 --> 학과, 학생 테이블 조인해서 sql문 실행
+		String sql = "SELECT S.STD_NO, S.STD_NAME, " + "M.MAJOR_NAME, S.STD_SCORE, S.STD_GENDER "
+				+ "FROM STUDENT S JOIN MAJOR M " + "ON S.MAJOR_NO = M.MAJOR_NO";
+
+		ArrayList<StudentDTO> list = new ArrayList<StudentDTO>();
+		try (Connection conn = ods.getConnection(); 
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs.next()) {
+					String studentNo = rs.getString(1);
+					String studentName = rs.getString(2);
+					String majorName = rs.getString(3);
+					double score = rs.getDouble(4);
+					char gender = rs.getString(5).charAt(0);
+					// 데이터를 꺼내서 StudentDTO 객체를 만들어서
+					// list에 등록
+					list.add(new StudentDTO(studentNo, studentName, 0,
+							majorName, score, gender));
+
 				}
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return list;
 	}
-	
+
 }
-
-
-
-
-
-
-
-
