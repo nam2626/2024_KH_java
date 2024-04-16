4d061c096954fecfc468a9ab9cfa344f47c6a290
elzenaro <nam2626@naver.com>
2024-04-16 10:13:25 +0900

    JDBC

--- jdbc/src/car/CarJDBCMain.java
+++ jdbc/src/car/CarJDBCMain.java
@@ -1,9 +1,41 @@
 package car;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import config.DBManager;
+
 public class CarJDBCMain {
 	public static void selectAllCar() {
-		//Car 테이블에 있는 모든 정보를 조회해서  출력
+		//Car 테이블에 있는 모든 정보를 조회해서 출력
 		//CAR_ID, CAR_NAME, CAR_MAKE_YEAR, CAR_PRICE, CAR_MAKER_NAME
+		String sql = "SELECT CAR_ID, CAR_NAME, CAR_MAKE_YEAR, "
+				+ "CAR_PRICE, CAR_MAKER_NAME "
+				+ "FROM CAR C JOIN CAR_MAKER CM "
+				+ "ON C.CAR_MAKER_CODE = CM.CAR_MAKER_CODE";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getInstance().getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1) 
+						+ " " + rs.getString(2)
+						+ " " + rs.getInt(3)
+						+ " " + rs.getInt(4)
+						+ " " + rs.getString(5)
+						);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.getInstance().close(conn, pstmt, rs);
+		}
 	}
 	public static void insertCar() {
 		//자동차 정보한건 입력 받아서 CAR 테이블에 저장
