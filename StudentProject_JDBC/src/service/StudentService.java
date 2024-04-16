22d894dcf6ac5fa2a91e02cdb2551b1d677855fe
elzenaro <nam2626@naver.com>
2024-04-16 14:16:43 +0900

    StudentProject JDBC

--- StudentProject_JDBC/src/service/StudentService.java
+++ StudentProject_JDBC/src/service/StudentService.java
@@ -10,20 +10,9 @@ import exception.StudentException;
 public class StudentService {
 	private static StudentService instance = new StudentService();
 	
-	private ArrayList<StudentDTO> list;
 
 	private StudentService() {
-		list = new ArrayList<StudentDTO>();
-		list.add(new StudentDTO("20201111", "김철수", "컴퓨터공학과", 3.4));
-		list.add(new StudentDTO("20201222", "이영희", "경영학과", 3.8));
-		list.add(new StudentDTO("20201333", "박민수", "수학과", 3.2));
-		list.add(new StudentDTO("20201444", "최유리", "영어영문학과", 3.6));
-		list.add(new StudentDTO("20201555", "강민준", "물리학과", 3.0));
-		list.add(new StudentDTO("20201666", "김지민", "화학공학과", 3.5));
-		list.add(new StudentDTO("20201777", "이예린", "생명과학과", 3.7));
-		list.add(new StudentDTO("20201888", "박찬호", "기계공학과", 3.9));
-		list.add(new StudentDTO("20201999", "이승엽", "전자공학과", 3.3));
-		list.add(new StudentDTO("20202000", "류현진", "컴퓨터소프트웨어학과", 3.1));
+
 	}
 	
 	public static StudentService getInstance() {
@@ -33,42 +22,20 @@ public class StudentService {
 	}
 	
 	public void addStudent(StudentDTO dto) throws StudentException {
-		if(list.contains(dto))//해당 데이터 있냐?
-			throw new StudentException("학생 데이터 등록에 실패하였습니다.");
-		list.add(dto);
+
 	}
 	
 	//학생 데이터 검색
 	public StudentDTO searchStudent(String studentNo) {
-		int result = list.indexOf(new StudentDTO(studentNo, null, null, 0));
-		
-		if(result == -1)
-			return null;
-		else
-			return list.get(result);
+		return null;
 	}
 	//학생 데이터 삭제
 	public boolean deleteStudent(String studentNo) {
-		int result = list.indexOf(new StudentDTO(studentNo, null, null, 0));
-
-		if(result == -1)
-			return false;
-		
-		list.remove(result);
 		return true;
 	}
 
-	public ArrayList<StudentDTO> getList() {
-		return list;
-	}
-	
 	public ArrayList<StudentDTO> filterStudent(){
 		ArrayList<StudentDTO> r = new ArrayList<StudentDTO>();
-		
-		for (StudentDTO dto : list) {
-			if(dto.getScore() >= 3.0)
-				r.add(dto);
-		}
 		
 		return r;
 	}
@@ -76,15 +43,8 @@ public class StudentService {
 	public ArrayList<StudentDTO> searchNameStudent(String name) throws StudentException {
 		ArrayList<StudentDTO> r = new ArrayList<StudentDTO>();
 		
-		for(int i=0;i<list.size();i++) {
-			//문자열 검색시 해당 문자열이 없으면 인덱스번호 -1을 리턴
-			if(list.get(i).getStudentName().indexOf(name) != -1)
-				r.add(list.get(i));
-		}
-		
 		if(r.isEmpty())
 			throw new StudentException(name+"에 대한 검색 결과가 없습니다.");
-		
 		
 		return r;
 	}
