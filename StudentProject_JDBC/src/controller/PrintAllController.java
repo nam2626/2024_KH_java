d27d42eb2c29be0f726fddf150f32bbe8cecc937
elzenaro <nam2626@naver.com>
2024-04-16 14:51:56 +0900

    StudentProject JDBC

--- StudentProject_JDBC/src/controller/PrintAllController.java
+++ StudentProject_JDBC/src/controller/PrintAllController.java
@@ -10,10 +10,11 @@ public class PrintAllController implements Controller {
 	@Override
 	public void execute() {
 		System.out.println("전체 학생정보를 출력합니다........");
-		ArrayList<StudentDTO> list = StudentService.getInstance().getList();
-		for(int i=0;i<list.size();i++) {
-			list.get(i).printInfo();
-		}
+		ArrayList<StudentDTO> list = 
+				StudentService.getInstance().selectAllStudent();
+//		for(int i=0;i<list.size();i++) {
+//			list.get(i).printInfo();
+//		}
 		System.out.println();
 	}
 }
